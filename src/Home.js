2941957ccfdcf79c31f00551acc74e5906411724
SHIVAM <yyyshivamyyy@gmail.com>
2021-07-04 10:26:27 +0530

    :zap: adding useEffect effects

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,31 @@
-import { useState, useEffect } from 'react';
-import BlogList from './BlogList';
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome Party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+  const [name, setName] = useState('mario');
 
-    const [name, setName] = useState('mario');
-    const toggleMode = () => {
-        const [isOff, setOff] = useState(true);
-    }
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  }, [name])
 
-    useEffect(()=> {
-        console.log('use effect ran');
-        console.log(name);
-    }, [name]);
-
-    return ( 
-        <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-        </div>
-     );
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
+    </div>
+  );
 }
  
 export default Home;
